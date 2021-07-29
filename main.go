a01bcc5384acc33407e5f607c4581fd87fc0dc96
Gomam0n <603934493@qq.com>
2021-07-29 20:51:46 +0800

    update

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 )
 
+var IP = "127.0.0.1"
 var serverIP = "127.0.0.1"
 var serverPort = 8888
 var clientPort = 9999
@@ -15,32 +16,19 @@ func main() {
 
 	serverAddr := serverIP + ":" + strconv.Itoa(serverPort)
 	conn, err := net.Dial("udp", serverAddr)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	checkError(err)
 
 	defer conn.Close()
 
-	clientAddress := "0.0.0.0:" + strconv.Itoa(clientPort)
-	udpAddr, err := net.ResolveUDPAddr("udp", clientAddress)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	udpAddr, err := getUDPAddr(clientPort)
+	checkError(err)
 
 	readConn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	checkError(err)
 	defer readConn.Close()
 
 	_, err = conn.Write([]byte(strconv.Itoa(clientPort)))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	checkError(err)
 
 	var content []byte
 	for {
@@ -59,11 +47,26 @@ func main() {
 			break
 		}
 	}
-	file, err := os.Create("receive.txt")
+	WriteFile("receive.txt", content)
+
+}
+func WriteFile(name string, content []byte) {
+	file, err := os.Create(name)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Fprintf(file, string(content))
+}
+func getUDPAddr(port int) (udpAddr *net.UDPAddr, err error) {
+	serverAddress := IP + ":" + strconv.Itoa(port)
+	udpAddr, err = net.ResolveUDPAddr("udp", serverAddress)
+	return
+}
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
